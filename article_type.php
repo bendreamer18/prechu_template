5d58cf026f3cfaaf13da4cda9635c1ee5d16187a
Ben Dreamer <bendjamin18@gmail.com>
2016-04-23 18:10:44 +0300

    Загружены скриншоты, прописано название страницы.

--- article_type.php
+++
@@ -1,9 +0,0 @@
-<?php if (have_posts()) : ?>                    
-  <?php     while (have_posts()) : the_post(); ?>
-
-							<!-- Код самой статьи -->
-
-<?php endwhile; ?>
-<div class="pagenavi"><?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } ?></div>
-<?php else : ?>
-<?php endif; ?>
